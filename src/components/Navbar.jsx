229425acc2c722d4180c756b401bfb5f978c8780
Ruslan <c8man24@gmail.com>
2023-01-17 21:29:33 +0300

    feat: implement navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,76 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import {
+  MdOutlineAccountCircle as AccountCircleIcon,
+  MdOutlineSearch as SearchIcon
+} from 'react-icons/md'
+
+const Container = styled.div`
+  position: sticky;
+  top: 0;
+  height: 56px;
+  font-size: 28px;
+  background-color: white;
+`
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  height: 100%;
+  padding: 0 35px;
+  justify-content: flex-end;
+  position: relative;
+`
+
+const Search = styled.div`
+  margin: auto;
+  position: absolute;
+  left:0;
+  right: 0;
+  width: 50%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border: 1px solid #ccc;
+  padding: 5px 15px;
+  border-radius: 20px;
+`
+
+const Input = styled.input`
+  border: none;
+  width: 95%;
+  outline: none;
+  font-size: 17px;
+`
+
+const Button = styled.button`
+  padding: 5px 15px;
+  background-color: transparent;
+  border: 1px solid #3ea6ff;
+  border-radius: 3px;
+  font-weight: 500;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  font-size: 17px;
+`
+
+const Navbar = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Search>
+          <Input placeholder='Search' />
+          <SearchIcon />
+        </Search>
+        <Button>
+          <AccountCircleIcon />
+            Sign in
+        </Button>
+      </Wrapper>
+    </Container>
+  )
+}
+
+export default Navbar
